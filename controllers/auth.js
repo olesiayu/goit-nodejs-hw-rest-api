8f8f3e9b30ad9ace2c613196d6c7d5bf3b8569d4
Olesia Yurchenko <olesya.yurchenko91@gmail.com>
2022-07-03 02:16:10 +0300

    finish hw 06

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,14 @@
 const authService = require("../services/auth.service");
+const emailService = require("../services/email.service");
+const userService = require("../services/user.service");
+const createError = require("../errors");
 
 const registerUser = async (req, res, next) => {
   try {
     const user = await authService.registerUser(req.body);
+
+    await emailService.sendEmail(user.email, user.verificationToken);
+
     res.json({
       email: user.email,
       subscription: user.subscription,
@@ -41,9 +47,58 @@ const currentUser = async (req, res, next) => {
   }
 };
 
+const confirm = async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await userService.findUser({ verificationToken });
+
+    if (!user) {
+      throw createError(404, "User not found");
+    }
+
+    const result = await userService.updateUser(user._id, {
+      verify: true,
+      verificationToken: null,
+    });
+
+    return res.status(200).json({
+      code: 200,
+      data: result,
+      message: "Verification successful",
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const resend = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await userService.findUser({ email });
+
+    if (!user) {
+      throw createError(404, "User not found");
+    }
+    if (user.verify) {
+      throw createError(400, "Verification has already been passed");
+    }
+
+    await emailService.sendEmail(user.email, user.verificationToken);
+
+    return res.status(200).json({
+      code: 200,
+      message: "Verification email sent",
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
   logoutUser,
   currentUser,
+  confirm,
+  resend,
 };
